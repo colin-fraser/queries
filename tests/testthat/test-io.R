18d15213990c0b79fac250d86ef31fe389b4f915
Colin Fraser <colin.r.fraser@gmail.com>
2020-08-09 16:45:49 -0700

    - params can have default values now - header in query is now optional

--- tests/testthat/test-io.R
+++ tests/testthat/test-io.R
@@ -1,14 +1,14 @@
 test_that("reading query", {
-  expect_s3_class(load_query("example", "."), "query_template")
-  expect_s3_class(load_query("example.sql"), "query_template")
+  expect_s3_class(query_load("example_with_defaults", "."), "query_template")
+  expect_s3_class(query_load("example_with_defaults.sql"), "query_template")
+  fs::dir_create("sql")
+  fs::file_copy("example_with_defaults.sql", "sql/example_with_defaults_copy.sql")
+  expect_s3_class(query_load("example_with_defaults_copy.sql"), "query_template")
+  fs::dir_delete("sql")
 })
 
 test_that("query_location works", {
-  fs::dir_create("sql")
-  expect_equal(queries_location(), "sql")
-  fs::dir_delete("sql")
-
   options(default_queries_location = "abc")
-  expect_equal(queries_location(), "abc")
+  expect_equal(queries_default_location(), "abc")
   options(default_queries_location = NULL)
 })
