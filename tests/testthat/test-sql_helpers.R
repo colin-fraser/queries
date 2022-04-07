3c5def03a60ec5c17a5a4d38d5db66a8e6ded324
Colin Fraser <colinfraser@fb.com>
2022-04-07 11:12:08 -0700

    remove named_vec_to_cols

--- tests/testthat/test-sql_helpers.R
+++ tests/testthat/test-sql_helpers.R
@@ -15,12 +15,6 @@ test_that("blank_if_null works", {
   expect_equal(blank_if_null(NULL), "")
 })
 
-test_that("named_vec_to_cols works", {
-  expect_equal(named_vec_to_cols(c(a = "A")), "A as a")
-  expect_equal(named_vec_to_cols(c(a = "A", b = "B")),
-                                 "A as a,\nB as b")
-})
-
 test_that("names_to_as", {
   expect_equal(names_to_as(c(a = "A")), "A as a")
   expect_equal(names_to_as(c(a = 'A', 'B')), c("A as a", "B"))
