4fcb1b00caf6f23ad0a5fbfe4786acad8979c02e
Colin Fraser <colinfraser@fb.com>
2022-04-07 09:01:33 -0700

    10 make column identifiers better (#12)
    * adds names_to_as argument to comma_join
    
    * Increment version number to 0.3.3

--- tests/testthat/test-sql_helpers.R
+++ tests/testthat/test-sql_helpers.R
@@ -6,6 +6,8 @@ test_that("comma_join works", {
     ", 'a', 'b',"
   )
   expect_equal(comma_join(NULL, TRUE, TRUE, TRUE), "")
+  expect_equal(comma_join(c(avg_sales = 'AVG(sales)', 'country')),
+               'AVG(sales) as avg_sales, country')
 })
 
 test_that("blank_if_null works", {
@@ -17,6 +19,11 @@ test_that("named_vec_to_cols works", {
   expect_equal(named_vec_to_cols(c(a = "A")), "A as a")
   expect_equal(named_vec_to_cols(c(a = "A", b = "B")),
                                  "A as a,\nB as b")
+})
+
+test_that("names_to_as", {
+  expect_equal(names_to_as(c(a = "A")), "A as a")
+  expect_equal(names_to_as(c(a = 'A', 'B')), c("A as a", "B"))
 })
 
 test_that("and_join works", {
